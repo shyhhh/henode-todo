6b44fa4539bf2f1779a9a573ea39883ad6acda55
shyhhh <hhhtheshy@gmail.com>
2023-04-24 00:26:53 +0800

    封装优化代码

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const fs = require("fs");
-const homedir = require("os").homedir();
-const home = process.env.HOME || homedir;
-const p = require("path");
-const dbPath = p.join(home, ".todo");
+const db = require("./db.js");
 
-module.exports.add = (title) => {
+module.exports.add = async (title) => {
   // 读取之前的任务
-  fs.readFile(dbPath, { flag: "a+" }, (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      let list;
-      try {
-        list = JSON.parse(data.toString());
-      } catch (error1) {
-        list = [];
-      }
-      console.log(list);
-      const task = {
-        title: title,
-        done: false,
-      };
-      list.push(task);
-      const string = JSON.stringify(list);
-      fs.writeFile(dbPath, string + "\n", (error2) => {
-        if (error2) {
-          console.log(error2);
-        }
-      });
-    }
-  });
+  const list = await db.read();
   // 往里面添加一个 title 任务
+  list.push({ title, done: false });
   // 存储任务到文件
+  await db.write(list);
 };
