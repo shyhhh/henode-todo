9f8489de0c2624f3c2bda42c6f90660fd3c77737
shyhhh <hhhtheshy@gmail.com>
2023-04-23 21:53:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const program = require("commander");
+
+program.option("-x, --xxx", "what the x");
+program
+  .command("add")
+  .description("add a task")
+  .action((...args) => {
+    const words = args.slice(0, -1).join(" ");
+    console.log(words);
+  });
+program
+  .command("clear")
+  .description("clear all task")
+  .action(() => {
+    console.log('this is clear')
+  });
+
+program.parse(process.argv);
