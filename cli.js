4890da444ec43ebdf6ce968b8fc3164c726b2822
shyhhh <hhhtheshy@gmail.com>
2023-04-23 23:30:21 +0800

    实现创建功能

---
+++ cli.js
@@ -0,0 +1,19 @@
+const program = require("commander");
+const api = require("./index.js");
+
+program.option("-x, --xxx", "what the x");
+program
+  .command("add")
+  .description("add a task")
+  .action((...args) => {
+    const words = args.slice(0, -1).join(" ");
+    api.add(words);
+  });
+program
+  .command("clear")
+  .description("clear all task")
+  .action(() => {
+    console.log("this is clear");
+  });
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const program = require("commander");
+const fs = require("fs");
+const homedir = require("os").homedir();
+const home = process.env.HOME || homedir;
+const p = require("path");
+const dbPath = p.join(home, ".todo");
 
-program.option("-x, --xxx", "what the x");
-program
-  .command("add")
-  .description("add a task")
-  .action((...args) => {
-    const words = args.slice(0, -1).join(" ");
-    console.log(words);
+module.exports.add = (title) => {
+  // 读取之前的任务
+  fs.readFile(dbPath, { flag: "a+" }, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let list;
+      try {
+        list = JSON.parse(data.toString());
+      } catch (error1) {
+        list = [];
+      }
+      console.log(list);
+      const task = {
+        title: title,
+        done: false,
+      };
+      list.push(task);
+      const string = JSON.stringify(list);
+      fs.writeFile(dbPath, string + "\n", (error2) => {
+        if (error2) {
+          console.log(error2);
+        }
+      });
+    }
   });
-program
-  .command("clear")
-  .description("clear all task")
-  .action(() => {
-    console.log('this is clear')
-  });
-
-program.parse(process.argv);
+  // 往里面添加一个 title 任务
+  // 存储任务到文件
+};
